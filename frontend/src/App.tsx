11a2e33b621304642b40c056e679a910ae4c8cc3
Lucas <ll947423@gmail.com>
2025-09-02 19:01:49 -0300

    feat: initialize frontend with React, Vite, and TypeScript. The Background already is implemented.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <div className='animated-background'>
+        <div className='moving-gradient'>
+        </div>
+          <div className='content'>
+            <p>Hello World</p>
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default App
