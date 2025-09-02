67ef1eecc068cdf1c8e751d298a5f827954dc245
Lucas <ll947423@gmail.com>
2025-09-02 16:15:41 -0300

    feat: implement initial Express server and Lead model interface

---
+++ backend/src/app.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import { setRoutes } from './routes/index';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+setRoutes(app);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
